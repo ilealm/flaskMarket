859fdf1893ed3c9ea20dc4eeea55eca028f1fdf7
Iris Leal <iris_leal_m@hotmail.com>
2021-04-15 11:37:47 -0700

    Added logout funcionality

--- market/routes.py
+++ market/routes.py
@@ -4,8 +4,8 @@ from market.models import Item, User
 
 from market.forms import RegisterForm, LoginForm
 from market import db
-# for login. Makes acccesible from other pages, eg. base.html
-from flask_login import login_user
+# for login/logout Makes acccesible from other pages, eg. base.html
+from flask_login import login_user, logout_user
 
 
 
@@ -66,3 +66,13 @@ def login_page():
           flash('Username and password are not match! Please try again', category='danger')
 
     return render_template('login.html', form=form)
+
+
+
+
+@app.route('/logout')
+def logout_page():
+    # built in fun in flask to logout users 
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("home_page"))
